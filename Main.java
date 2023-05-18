b49b83961af7260dbd58433663506353fa0ab1fc
rafterco <rafterco@users.noreply.github.com>
2023-05-18 15:24:10 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class Main {
     public static void main(String[] args) {
-        UnixScriptExecutor executor = new UnixScriptExecutor();
-        executor.executeScript("/path/to/your/script.sh", "/path/to/your/file.txt");
+        // Create a sample file in memory
+        String fileContent = "This is a sample file content.";
+        byte[] fileData = fileContent.getBytes();
+
+        // Specify the path to save the file
+        String filePath = "/path/to/your/file.txt";
+
+        try {
+            // Write the file data to the filesystem
+            Path outputFile = Path.of(filePath);
+            Files.write(outputFile, fileData, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            System.out.println("File created: " + filePath);
+
+            // Create an instance of UnixScriptExecutor
+            UnixScriptExecutor executor = new UnixScriptExecutor();
+
+            // Execute the script
+            executor.executeScript("/path/to/your/script.sh", filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
