ec9dbeef045ce2f40d96364fc83346ea86fbe3f3
rafterco <rafterco@users.noreply.github.com>
2023-05-18 14:55:25 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,18 +6,30 @@ echo "Executing script.sh"
 if [ -f "file_to_transfer.txt" ]; then
   echo "File exists."
 
-  # Attempt to SFTP the file
-  sftp user@hostname <<EOF
-    put file_to_transfer.txt
-    quit
-EOF
+  # Run another command before SFTP
+  ls -l file_to_transfer.txt
 
   # Check the exit status of the previous command
   if [ $? -eq 0 ]; then
-    echo "SFTP successful."
-    exit 0  # Success
+    echo "Command successful. Proceeding with SFTP."
+
+    # Attempt to SFTP the file
+    sftp user@hostname <<EOF
+      put file_to_transfer.txt
+      quit
+EOF
+
+    # Check the exit status of the previous command
+    if [ $? -eq 0 ]; then
+      echo "SFTP successful."
+      exit 0  # Success
+    else
+      echo "SFTP failed."
+      exit 1  # Failure
+    fi
+
   else
-    echo "SFTP failed."
+    echo "Command failed. Cannot proceed with SFTP."
     exit 1  # Failure
   fi
 
